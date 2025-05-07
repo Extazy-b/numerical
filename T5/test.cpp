09355493959a7ad17efa3c75fa576073d696f841
Extazy-b <esikov506@gmail.com>
2025-05-07 11:09:16 +0300

    3. conj

--- T5/test.cpp
+++ T5/test.cpp
@@ -3,15 +3,5 @@
 using namespace std;
 
 int main(){
-    int N;
-    cin >> N;
-    vector <vector <size_t>> perms = permutations(N);
-    for (vector <size_t> perm: perms)
-    {
-        for (size_t num: perm) {
-            cout << num << ' ';
-        }
-        cout << endl;
-    }
-    return 0;
+    
 };
